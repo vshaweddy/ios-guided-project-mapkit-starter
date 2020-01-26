2e94d4d00b84c4ff745b12bb1f1968e83641dcc7
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-25 19:53:50 -0500

    adding alert

--- Quakes/Quakes/EarthquakesViewController.swift
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -8,14 +8,43 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class EarthquakesViewController: UIViewController {
+class EarthquakesViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 		
 	// NOTE: You need to import MapKit to link to MKMapView
 	@IBOutlet var mapView: MKMapView!
+    
+    private let locationManager = CLLocationManager()
+    private var userTrackingButton: MKUserTrackingButton!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+        
+        userTrackingButton = MKUserTrackingButton(mapView: mapView)
+        userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userTrackingButton)
+        
+        userTrackingButton.leftAnchor.constraint(equalTo: mapView.leftAnchor, constant: 20.0).isActive = true
+        userTrackingButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -20.0).isActive = true
+        
+        self.mapView.showsUserLocation = true
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
 	}
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView.setCenter(userLocation.coordinate, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        }
+    }
 }
